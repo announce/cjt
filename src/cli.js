11f1885e472bb4609a40f46c0dc2c803531e1e47
Kenta Yamamoto <lq33xxx@gmail.com>
2018-06-09 23:29:27 +0900

    Added cli script

---
+++ src/cli.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const fs = require('fs')
+const argparse = require('argparse')
+const cjt = require('cjt')
+
+const printJt = (err, data) => {
+  if (err) throw err
+  const jt = cjt.convert(data)
+  console.log(jt)
+}
+const parser = new argparse.ArgumentParser({
+  version: process.env.npm_package_version,
+  addHelp: true,
+  description: process.env.npm_package_description
+})
+parser.addArgument(
+  [ '-f', '--file' ],
+  {
+    help: 'Specify target file',
+    required: true
+  }
+)
+parser.addArgument(
+  [ '--encoding' ],
+  {
+    help: 'Specify encoding to read file',
+    argumentDefault: 'utf8'
+  }
+)
+const args = parser.parseArgs()
+fs.readFile(
+  args.file,
+  printJt,
+  args.encoding
+)
